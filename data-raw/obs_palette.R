5a1d2f5c3d310b8c992de5b30b2639a5f58a881e
pabsantos <pabsantos21@gmail.com>
2023-07-14 17:15:59 -0300

    Create `obs_palette` dataset

---
+++ data-raw/obs_palette.R
@@ -0,0 +1,10 @@
+obs_palette <- list(
+  blue = "#00496d",
+  yellow = "#f7951d",
+  red = "#d51f29",
+  green = "#1fa149",
+  pink = "#ec0d6b",
+  orange = "#f05f22"
+)
+
+usethis::use_data(obs_palette, overwrite = TRUE)
