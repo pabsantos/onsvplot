114b28f90e629fe6c941982a5208681cb0b9b4c4
pabsantos <pabsantos21@gmail.com>
2023-07-14 17:36:26 -0300

    Document `scale_discrete_obs.R`

--- R/scale_discrete_obs.R
+++ R/scale_discrete_obs.R
@@ -1,3 +1,18 @@
+#' Apply ONSV color scale to ggplot object
+#'
+#' `scale_discrete_obs()` is used to apply the ONSV color scale to a
+#' ggplot graphics
+#'
+#' @return A custom scale object for ggplot objects
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#'
+#' ggplot(data = iris, aes(x = Sepal.Width, y = Sepal.Length, color = Species)) +
+#'   geom_point() +
+#'   theme_obs() +
+#'   scale_discrete_obs()
 scale_discrete_obs <- function() {
   palette <- c("#00496d", "#f7951d", "#d51f29", "#1fa149", "#ec0d6b", "#f05f22")
   ggplot2::scale_discrete_manual(
