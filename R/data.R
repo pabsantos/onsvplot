314ee0d5b3a51e4ab4d3e0fcdcfbabe45b6539c8
pabsantos <pabsantos21@gmail.com>
2023-07-14 19:03:37 -0300

    Document `obs_palette`

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' ONSV Color Palette
+#'
+#' A list including the hex code of each color used in ONSV style
+#'
+#' @format ## `obs_palette`
+#' A list with 6 colors: blue, yellow, red, green, pink, and orange
+#'
+#' @source <https://onsv.org.br>
+
+"obs_palette"
