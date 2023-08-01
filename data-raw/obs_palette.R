c20af223dd59cde2aa4c5f345b3506fe738708d4
pabsantos <pabsantos21@gmail.com>
2023-08-01 10:17:49 -0300

    Add new colors to `obs_palette`

--- data-raw/obs_palette.R
+++ data-raw/obs_palette.R
@@ -4,7 +4,13 @@ obs_palette <- list(
   red = "#d51f29",
   green = "#1fa149",
   pink = "#ec0d6b",
-  orange = "#f05f22"
+  orange = "#f05f22",
+  lightblue = "#99B6C5",
+  lightyellow = "#FCD5A5",
+  lightred = "#EEA5A9",
+  lightgreen = "#A5D9B6",
+  lightpink = "#F79EC4",
+  lightorange = "#F9BFA7"
 )
 
 usethis::use_data(obs_palette, overwrite = TRUE)
