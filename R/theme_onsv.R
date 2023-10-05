f080fd4aee05a30229fc3626769b7e10c1f94948
pabsantos <pabsantos21@gmail.com>
2023-10-05 11:58:18 -0300

    Updates legend size in `theme_onsv`(#13 and #14)

--- R/theme_onsv.R
+++ R/theme_onsv.R
@@ -42,21 +42,22 @@ theme_onsv <- function(basesize = 10) {
     legend.background = ggplot2::element_blank(),
     legend.title = ggplot2::element_blank(),
     legend.key = ggplot2::element_blank(),
+    legend.key.size = unit(0.3, "cm"),
     legend.text = ggplot2::element_text(
       family = font,
-      size = basesize,
+      size = basesize - 1,
       color = "grey20"
     ),
     legend.margin = ggplot2::margin(t = 0, r = 0, b = 0, l = -10),
     axis.title = ggplot2::element_text(
       family = font,
-      size = basesize,
+      size = basesize - 1,
       color = "grey10"
     ),
     axis.ticks = ggplot2::element_blank(),
     axis.text = ggplot2::element_text(
       family = font,
-      size = basesize - 1,
+      size = basesize - 2,
       color = "grey30"
     ),
     panel.grid.minor = ggplot2::element_blank(),
